590c516d853a79cc6cb003ea217d46046699cfe8
froy lim <lim.froy@gmail.com>
2016-02-23 11:22:57 +0800

    Set active navigation link

--- src/AppsPortal/wwwroot/js/site.js
+++ src/AppsPortal/wwwroot/js/site.js
@@ -21,7 +21,7 @@
     //{
     //    var me = $(this);
     //    alert(me.text());
-    //});
+    //});    
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
     var $icon = $("#sidebarToggle i.fa");
@@ -38,9 +38,11 @@
             $icon.removeClass("fa-angle-right");
             $icon.addClass("fa-angle-left");
         }
-    });
+    });   
+        
 })();
 
+$('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
 //main.style= "background-color: #888;";
 //main.style = "";
 //startup();
